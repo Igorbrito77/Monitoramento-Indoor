0e37e2b4538b8826d9460144d002c238f407c60b
Igor Brito <britoigor77@gmail.com>
2020-12-05 09:29:05 -0300

    subindo script principal

---
+++ script.py
@@ -0,0 +1,16 @@
+## recebe string do PR
+nome = input('Insira o nome do Ponto de Referencia: \n')
+
+
+
+##Gerar hash de numero de 32 bits com o nome do PR
+
+
+# hash32 = lambda value: hash('PR') & 0xffffffff
+
+
+## concatenar com o sufixo 
+
+sufixo = '0000000000000000'
+
+	
